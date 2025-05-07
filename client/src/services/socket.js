fbd010bee3a20b003e11f032a0c5bdb21fe42d7e
Taha Ashraf Ansari <129264109+tahauchiha@users.noreply.github.com>
2025-05-07 13:30:18 +0530

    Update socket.js

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -2,6 +2,6 @@
 import io from 'socket.io-client';
 
 // Create a single socket instance
-const socket = io('https://live-poll-app-server.vercel.app/');
+const socket = io('https://live-poll-app-server.onrender.com/');
 
 export default socket;
